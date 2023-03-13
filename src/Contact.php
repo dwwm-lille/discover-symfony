36f1147e691f206dabed6630a252f47e7e6bfdcf
Matthieu Mota <matthieumota@gmail.com>
2023-03-13 12:31:14 +0100

    Exo form : create model

---
+++ src/Contact.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+class Contact
+{
+    private $name;
+    private $email;
+    private $message;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
